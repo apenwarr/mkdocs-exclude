afb5f8a5c465e638235f20f1dab1452a7eb7b526
Avery Pennarun <apenwarr@gmail.com>
2019-01-07 14:21:58 -0500

    Bump version number because I uploaded it wrong to pip.
    Apparently you need a really new version of wheel+twine if you want
    the README to render correctly in markdown format.  Sure, why not.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(name):
 
 setuptools.setup(
     name='mkdocs-exclude',
-    version='1.0.0',
+    version='1.0.1',
     packages=['mkdocs_exclude'],
     url='https://github.com/apenwarr/mkdocs-exclude',
     license='Apache',
